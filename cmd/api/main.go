539151b14ec3a3720dbe7dd4ddd85fcb1504ca8e
Harry Nguyen <harrynguyen2287@gmail.com>
2023-01-06 09:58:33 +0700

    Health check

---
+++ cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var config config
+
+	flag.IntVar(&config.port, "port", 4000, "API server port")
+	flag.StringVar(&config.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: config,
+		logger: logger,
+	}
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("Starting %s server on %s", config.env, server.Addr)
+	err := server.ListenAndServe()
+	logger.Fatal(err)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/harryng22/moviedb
 
 go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect

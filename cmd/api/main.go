79793790dd65ea135d57a6d1e3096bf87d602b8f
Harry Nguyen <harrynguyen2287@gmail.com>
2023-01-09 09:01:06 +0700

    Set db connection pool options

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -61,6 +61,15 @@ func openDB(config Config) (*sql.DB, error) {
 		return nil, err
 	}
 
+	db.SetMaxOpenConns(config.DbMaxOpenConns)
+	db.SetMaxIdleConns(config.DbMaxIdleConns)
+
+	duration, err := time.ParseDuration(config.DbMaxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 

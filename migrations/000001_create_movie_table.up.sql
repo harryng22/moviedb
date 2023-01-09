f4db95158e7d712288864b418301916df2b62c34
Harry Nguyen <harrynguyen2287@gmail.com>
2023-01-09 11:13:38 +0700

    Add and apply migrations

---
+++ migrations/000001_create_movie_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS movie (
+    id BIGSERIAL PRIMARY KEY,
+    created_at TIMESTAMP(0) with TIME ZONE NOT NULL DEFAULT NOW(),
+    title TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    runtime INTEGER NOT NULL,
+    genres TEXT[] NOT NULL,
+    version INTEGER NOT NULL DEFAULT 1
+);

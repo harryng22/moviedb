a3402abd13286d5cc95098480d2cdbeeaa454f41
Harry Nguyen <harrynguyen2287@gmail.com>
2023-01-11 14:30:01 +0700

    Sorting list

--- internal/data/filter.go
+++ internal/data/filter.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/harryng22/moviedb/internal/validator"
+import (
+	"strings"
+
+	"github.com/harryng22/moviedb/internal/validator"
+)
 
 type Filter struct {
 	Page         int
@@ -15,4 +19,22 @@ func ValidateFilter(v *validator.Validator, f Filter) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+func (f Filter) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filter) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
 }
